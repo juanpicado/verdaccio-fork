e208df050ed7d1ae62bfe08073f6202066d1370a
Juan Picado <juanpicado19@gmail.com>
2022-05-13 18:53:21 +0200

    fix test

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -25,13 +25,13 @@ test("foo has access to jquery", (done) => {
   done();
 });
 
-test('verify publish a package', (done) => {
+test("verify publish a package", (done) => {
   login("foo", "bar", port.toString());
   const object = runNpmPublish(port.toString());
-  const {name, version} = require('../package.json');
+  const { name, version } = require("../package.json");
   const pkgName = `${name}@${version}`;
   const info = runNpmInfo(pkgName, port.toString()) as any;
   expect(info.name).toBe(name);
-  expect(info.version).toBe('12.0.0');
+  expect(info.version).toBe("1.0.0");
   done();
 });
